908e1dfe6ab54a4620494c70b190f1ec10392493
Doriajosea <nikiemadoriajoseannick@gmail.com>
2023-10-18 15:51:08 -0400

    connect3

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+require_once('functions/functions.php');
 $server = 'localhost';
 $userName = "root";
 $pwd = "";
@@ -63,8 +65,66 @@ echo "</br>";
 echo "</br>";
 var_dump($data3);
 
+// recuperer toutes les lignes de la table user
+
+// avec fetch assoc
+$result4 = mysqli_query($conn, "SELECT user_name, email, id FROM user");
+
+$data4 = [];
+$i = 0;
+while ($rangeeData = mysqli_fetch_assoc($result4)) {
+    $data4[$i] = $rangeeData;
+    $i++;
 
 
+    echo "</br>";
+    echo "</br>";
+    echo "Nom de l'usager :" . $rangeeData["user_name"] . "</br>";
+    echo "Courriel :" . $rangeeData["email"] . "</br>";
 
+};
+
+echo "</br>";
+echo "</br>";
+echo "Quatrieme fetch";
+echo "</br>";
+echo "</br>";
+echo "Mon array aura : " . mysqli_num_rows($result4) . "lignes.";
+echo "</br>";
+echo "</br>";
+var_dump($result4);
+echo "</br>";
+echo "</br>";
+var_dump($data4);
+
+
+$result6 = mysqli_query($conn, "SELECT * FROM user");
+
+$data6 = [];
+$i = 0;
+while ($rangeeData = mysqli_fetch_row($result6)) {
+    $data6[$i] = $rangeeData;
+    $i++;
+
+
+    echo "</br>";
+    echo "</br>";
+    echo "Nom de l'usager :" . $rangeeData[1] . "</br>";
+    echo "Courriel :" . $rangeeData[2] . "</br>";
+
+};
+
+echo "</br>";
+echo "</br>";
+echo "Sixieme fetch";
+echo "</br>";
+echo "</br>";
+echo "Mon array aura : " . mysqli_num_rows($result6) . "lignes.";
+echo "</br>";
+echo "</br>";
+var_dump($result6);
+echo "</br>";
+echo "</br>";
+var_dump($data6);
 
 ?>
